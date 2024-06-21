57e8cc725f2abb968e6f68e68e8da44b41eae95c
joshtrinh1102 <nltrinh1102@gmail.com>
2024-06-20 21:39:43 -0400

    Update example.js

--- example.js
+++ example.js
@@ -2,14 +2,14 @@ function main(){
     var bardata=[100, 420, 230, 850, 560, 925]
     var width = 500;
     var barHeight = 20;
-    margin=1
+    var margin=1
 
 
     var graph= d3.select("body")
                     .append("svg")
                     .attr("width",width)
                     .attr("height", barHeight * bardata.length)
-    var scale= d3.scaleLinear()
+    var xScale= d3.scaleLinear()
                     .domain([d3.min(bardata), d3.max(bardata)])
                     .range([50,500])
     var bar= graph.selectAll("g")
@@ -20,43 +20,30 @@ function main(){
                     });
     bar.append("rect")
             .attr('width',function(d){
-            return scale(d) ;
+            return xScale(d) ;
     })                
             .attr('height', barHeight -margin)///Render bars and group elements based on scaled data
     bar.append('text')
             .attr('x', function(d){
-            return (scale(d));//Add text labels to bars
+            return (xScale(d));s
     })
             .attr('y', barHeight /2)
             .attr('dy', '.35em')
-            .text(function(d) {return d;});
+            .text(function(d) {return d;});//Add text labels to bar
 
-    bar.selectAll(".bar")
+    bar.selectAll("rect")
     //Implement transitions and hover effects for bars
-    .on("mouseover",onMouseOver)
-    .on("mouseout",onMouseOut)
-    .transition()
-    .ease(d3.easeLinear)
-    .duration(500)
-    .delay(function(d,i){return i * 50})
-    //mouse over
-    function onMouseOver(d,i){
-        d3.select(this).attr('class','highlight')
-        d3.select(this)
+        .on("mouseover", function() {
+            d3.select(this)
             .transition()
-            .duration(500)
-            .attr('width', xScale.bandwidth()+5)
-            .attr('y', function(d){return yScale(d.value)-10;})
-            .attr('height', function(d){return height - yScale(d.value) + 10;})
-    }
-    //mouseout
-    function onMouseOut(d, i){
-        d3.select(this).attr('class','bar')
+            .duration(300)
+            .attr("fill", "greenyellow"); 
+    })
+        .on("mouseout", function() {
         d3.select(this)
-                .transition()
-                .duration(500)
-                .attr('width', xScale.bandwidth())
-                .attr('y', function(d){return yScale(d.value);})
-                .attr('height', function(d){return height - yScale(d.value);})
-    }
-    }
+          .transition()
+          .duration(300)
+          .attr("fill", "turquoise"); 
+
+    })
+}
